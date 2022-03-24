590fdd07f04679cad33aa0dbff94cd5b7327a16e
Wleed Salah <87587845+DevWaleedS@users.noreply.github.com>
2022-03-25 00:24:23 +0200

    javascript commit

---
+++ js/discover-js.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    rtl:true,
+    margin: 10,
+    dots:false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        700: {
+            items: 2
+        },
+        800: {
+            items: 3
+        },
+        1000: {
+            items: 4
+        }
+    }
+});
---
+++ js/js.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    rtl:true,
+    margin: 10,
+    dots:false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        700: {
+            items: 3
+        },
+        800: {
+            items: 5
+        },
+        1000: {
+            items: 7
+        }
+    }
+});
